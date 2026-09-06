4dc1cc4800e0b1d45311f303285a710741b17d40
vkdev456 <kviswas1216@gmail.com>
2026-09-06 10:40:41 +0530

    first commit

---
+++ backend/src/main/java/com/repoly/backend/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.repoly.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+import com.repoly.backend.dto.SignupDto;
+
+@RestController
+public class UserController {
+
+   
+}
---
+++ backend/src/main/java/com/repoly/backend/services/UserService.java
@@ -0,0 +1,5 @@
+package com.repoly.backend.services;
+
+public class UserService {
+    
+}
